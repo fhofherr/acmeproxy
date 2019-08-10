990f42951487ae67c7ee782f7b9a7a00dc9adaf3
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-08-10 13:27:37 +0200

    Read private keys from io.Reader
    The capability to read private keys from an arbitrary io.Reader is
    useful during testing. It becomes necessary when writing keys to a
    data store.

--- pkg/certutil/key.go
+++ pkg/certutil/key.go
@@ -6,6 +6,10 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"io"
+	"io/ioutil"
 
 	"github.com/pkg/errors"
 )
@@ -53,4 +57,54 @@ func NewPrivateKey(kt KeyType) (crypto.PrivateKey, error) {
 		return nil, errors.Errorf("unknown key type: %v", kt)
 	}
 	return pk, errors.Wrap(err, "new private key")
+}
+
+// ReadPrivateKey reads an private key from r using either ReadECDSAPrivateKey
+// or ReadRSAPrivateKey.
+//
+// The value of kt determines which the type of key to be read. To read an
+// ECDSA private key any of the EC* values can be used. Likewise to read an RSA
+// private key any of the RSA* values can be passed.
+func ReadPrivateKey(kt KeyType, r io.Reader, pemDecode bool) (crypto.PrivateKey, error) {
+	var (
+		pk  crypto.PrivateKey
+		err error
+	)
+	switch kt {
+	case EC256, EC384:
+		pk, err = readKey(r, pemDecode, parseECDSAKey)
+	case RSA2048, RSA4096, RSA8192:
+		pk, err = readKey(r, pemDecode, parseRSAKey)
+	default:
+		return nil, errors.Errorf("invalid key type: %v", kt)
+	}
+	return pk, errors.Wrap(err, "read private key")
+}
+
+func readKey(r io.Reader, pemDecode bool, df func([]byte) (crypto.PrivateKey, error)) (crypto.PrivateKey, error) {
+	bs, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "read key data")
+	}
+	if pemDecode {
+		block, rest := pem.Decode(bs)
+		if block == nil {
+			return nil, errors.New("empty PEM block")
+		}
+		if len(rest) > 0 {
+			return nil, errors.New("found more than one PEM block")
+		}
+		bs = block.Bytes
+	}
+	return df(bs)
+}
+
+func parseECDSAKey(bs []byte) (crypto.PrivateKey, error) {
+	key, err := x509.ParseECPrivateKey(bs)
+	return key, errors.Wrap(err, "parse ECDSA private key")
+}
+
+func parseRSAKey(bs []byte) (crypto.PrivateKey, error) {
+	key, err := x509.ParsePKCS1PrivateKey(bs)
+	return key, errors.Wrap(err, "parse RSA private key")
 }
--- pkg/certutil/key_test.go
+++ pkg/certutil/key_test.go
@@ -1,11 +1,17 @@
 package certutil_test
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/rsa"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/fhofherr/acmeproxy/pkg/certutil"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -38,4 +44,89 @@ func TestNewPrivateKeyInvalidKeyType(t *testing.T) {
 	keyType := certutil.KeyType(-1)
 	_, err := certutil.NewPrivateKey(keyType)
 	assert.Error(t, err)
+}
+
+func TestReadPrivateKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		keyType certutil.KeyType
+		pem     bool
+	}{
+		{"ec256.pem", certutil.EC256, true},
+		{"ec256.der", certutil.EC256, false},
+		{"ec384.pem", certutil.EC384, true},
+		{"ec384.der", certutil.EC384, false},
+		{"rsa2048.pem", certutil.RSA2048, true},
+		{"rsa2048.der", certutil.RSA2048, false},
+		{"rsa4096.der", certutil.RSA4096, false},
+		{"rsa8192.der", certutil.RSA8192, false},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			keyPath := filepath.Join("testdata", t.Name())
+			if *certutil.FlagUpdate {
+				certutil.CreateOpenSSLPrivateKey(t, keyPath)
+			}
+			r, err := os.Open(keyPath)
+			if !assert.NoError(t, err) {
+				return
+			}
+			_, err = certutil.ReadPrivateKey(tt.keyType, r, tt.pem)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestReadPrivateKeyInvalidKeyType(t *testing.T) {
+	keyType := certutil.KeyType(-1)
+	r := &bytes.Buffer{}
+	_, err := certutil.ReadPrivateKey(keyType, r, false)
+	assert.Error(t, err)
+}
+
+func TestReadPrivateKeyReaderError(t *testing.T) {
+	expectedErr := errors.New("expected error")
+	r := &errorReader{expectedErr}
+	_, err := certutil.ReadPrivateKey(certutil.EC256, r, false)
+	assert.Error(t, err)
+	assert.Equal(t, expectedErr, errors.Cause(err))
+}
+
+func TestReadPrivateKeyInvalidPEMBlock(t *testing.T) {
+	r := strings.NewReader("invalid PEM data")
+	_, err := certutil.ReadPrivateKey(certutil.RSA2048, r, true)
+	assert.Error(t, err)
+}
+
+func TestReadConcatenatedPEMBlocks(t *testing.T) {
+	certFiles := []string{
+		filepath.Join("testdata", t.Name(), "ec256_1.pem"),
+		filepath.Join("testdata", t.Name(), "ec256_2.pem"),
+	}
+	if *certutil.FlagUpdate {
+		for _, path := range certFiles {
+			certutil.CreateOpenSSLPrivateKey(t, path)
+		}
+	}
+	pemBytes := make([]byte, 0, 1024)
+	for _, path := range certFiles {
+		bs, err := ioutil.ReadFile(path)
+		if !assert.NoError(t, err) {
+			return
+		}
+		pemBytes = append(pemBytes, bs...)
+	}
+	r := bytes.NewReader(pemBytes)
+	_, err := certutil.ReadPrivateKey(certutil.EC256, r, true)
+	assert.Error(t, err)
+}
+
+type errorReader struct {
+	Err error
+}
+
+func (e *errorReader) Read([]byte) (int, error) {
+	return 0, e.Err
 }
