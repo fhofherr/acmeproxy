e577d4234d2fb0aab93f92969cc07f44d50a9943
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-09-01 15:21:51 +0200

    Use bbolt when starting the server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,10 +3,11 @@ package server
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
 	"github.com/fhofherr/acmeproxy/pkg/acme"
 	"github.com/fhofherr/acmeproxy/pkg/acme/acmeclient"
-	"github.com/fhofherr/acmeproxy/pkg/acme/acmetest"
+	"github.com/fhofherr/acmeproxy/pkg/db"
 	"github.com/fhofherr/acmeproxy/pkg/httpapi"
 	"github.com/fhofherr/golf/log"
 	"github.com/google/uuid"
@@ -16,6 +17,7 @@ import (
 type Config struct {
 	ACMEDirectoryURL string
 	HTTPAPIAddr      string
+	DataDir          string
 	Logger           log.Logger
 }
 
@@ -23,18 +25,23 @@ type Config struct {
 type Server struct {
 	httpAPIServer *httpServer
 	acmeAgent     *acme.Agent
+	boltDB        *db.Bolt
 	logger        log.Logger
 }
 
 // New creates a new Server and configures it using cfg.
 func New(cfg Config) (*Server, error) {
+	boltDB := &db.Bolt{
+		FilePath: filepath.Join(cfg.DataDir, "acmeproxy.db"),
+		FileMode: 0600,
+	}
 	acmeClient := &acmeclient.Client{
 		DirectoryURL: cfg.ACMEDirectoryURL,
 		HTTP01Solver: acmeclient.NewHTTP01Solver(),
 	}
 	acmeAgent := &acme.Agent{
-		Domains:      &acmetest.InMemoryDomainRepository{},
-		Clients:      &acmetest.InMemoryClientRepository{},
+		Domains:      boltDB.DomainRepository(),
+		Clients:      boltDB.ClientRepository(),
 		Certificates: acmeClient,
 		ACMEAccounts: acmeClient,
 	}
@@ -48,12 +55,18 @@ func New(cfg Config) (*Server, error) {
 	return &Server{
 		httpAPIServer: httpAPIServer,
 		acmeAgent:     acmeAgent,
+		boltDB:        boltDB,
 		logger:        cfg.Logger,
 	}, nil
 }
 
 // Start starts the Server without blocking.
 func (s *Server) Start() {
+	err := s.boltDB.Open()
+	if err != nil {
+		// TODO (fhofherr) panic is not an option for v0.1.0. But it is good enough for testing
+		panic("open bolt db")
+	}
 	s.httpAPIServer.Start()
 	s.registerAcmeproxyDomain()
 }
@@ -61,11 +74,12 @@ func (s *Server) Start() {
 // Shutdown performs a graceful shutdown of the Server.
 func (s *Server) Shutdown(ctx context.Context) {
 	s.httpAPIServer.Shutdown(ctx)
+	s.boltDB.Close()
 }
 
 func (s *Server) registerAcmeproxyDomain() {
 	tmpClientID := uuid.Must(uuid.NewRandom())
-	// TODO(fhofherr): make acmeproxy admin mail configurable
+	// TODO (fhofherr) make acmeproxy admin mail configurable
 	err := s.acmeAgent.RegisterClient(tmpClientID, "")
 	if err != nil {
 		log.Log(s.logger,
@@ -73,7 +87,7 @@ func (s *Server) registerAcmeproxyDomain() {
 			"error", err,
 			"message", "register default client")
 	}
-	// TODO(fhofherr): make acmeproxy domain configurable
+	// TODO (fhofherr) make acmeproxy domain configurable
 	err = s.acmeAgent.RegisterDomain(tmpClientID, "www.example.com")
 	if err != nil {
 		log.Log(s.logger,
