ee56b267a005f692da70a908fb2ff4304d372308
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-11-26 08:23:39 +0100

    Restructure api.Server start-up
    The api.Server needs to start multiple components in separate Go
    routines. Additionally it needs to obtain a TLS certificate for itself
    from Let's Encrypt. The previous start-up implementation did not take
    this into account. The new structure should do the trick, but is not yet
    complete. TODO comments document what needs to be done. They will be
    addressed in further commits.

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/fhofherr/acmeproxy/pkg/acme"
 	"github.com/fhofherr/acmeproxy/pkg/acme/acmeclient"
@@ -21,9 +23,6 @@ import (
 // Server runs the ACME Agent responsible of obtaining certificates and storing
 // them for later retrieval. Users connect to the server through its public
 // API to retrieve their certificates.
-//
-// The zero value of Server represents a valid instance. Server may start
-// a multitude of Go routines.
 type Server struct {
 	ACMEDirectoryURL string
 	HTTPAPIAddr      string
@@ -32,6 +31,9 @@ type Server struct {
 	httpAPIServer    *httpapi.Server
 	acmeAgent        *acme.Agent
 	boltDB           *db.Bolt
+
+	once    sync.Once
+	initErr error
 
 	// Accessed atomically. A non-zero value means the server is currently
 	// starting or has already been started. It cannot be started again.
@@ -44,70 +46,80 @@ type Server struct {
 // -- even if it has been stopped in the meantime by calling Shutdown -- leads
 // to an error.
 func (s *Server) Start() error {
-	const op errors.Op = "server/server.Start"
+	const op errors.Op = "api/server.Start"
+
+	if err := s.startOnce(); err != nil {
+		return errors.New(op, err)
+	}
+
+	return nil
+}
+
+func (s *Server) startOnce() error {
+	const op errors.Op = "api/server.startOnce"
+
+	s.once.Do(func() {
+		acmeclient.InitializeLego(s.Logger)
+		if s.initErr = s.initDB(); s.initErr != nil {
+			return
+		}
+		acmeClient := &acmeclient.Client{
+			DirectoryURL: s.ACMEDirectoryURL,
+		}
+		s.acmeAgent = &acme.Agent{
+			Domains:      s.boltDB.DomainRepository(),
+			Users:        s.boltDB.UserRepository(),
+			Certificates: acmeClient,
+			ACMEAccounts: acmeClient,
+		}
+		s.httpAPIServer = &httpapi.Server{
+			Solver: &acmeClient.HTTP01Solver,
+		}
+		if s.initErr = s.startHTTPAPI(); s.initErr != nil {
+			return
+		}
+		// TODO make acmeproxy domain configurable (#41)
+		if s.initErr = s.registerAcmeproxyDomain(); s.initErr != nil {
+			return
+		}
+		// TODO obtain certificates for gRPC API
+		// TODO make public key for JWTs configurable (#41)
+		// TODO start gRPC API
+	})
 
 	if !atomic.CompareAndSwapUint32(&s.started, 0, 1) {
 		return errors.New(op, "already started")
 	}
-	s.initialize()
-
-	if err := s.boltDB.Open(); err != nil {
-		return errors.New(op, err)
-	}
-	go errors.LogFunc(s.Logger, func() error {
-		err := netutil.ListenAndServe(s.httpAPIServer, netutil.WithAddr(s.HTTPAPIAddr))
-		return errors.Wrap(err, op)
-	})
-
-	if err := s.registerAcmeproxyDomain(); err != nil {
-		return errors.New(op, err)
-	}
-	return nil
+	return s.initErr
 }
 
-// isStarted returns true if an attempt to start the server has been made. A
-// true return value does not indicate the server is actually running. Start
-// may have failed with an error, or Shutdown was called in the meantime.
-func (s *Server) isStarted() bool {
-	return atomic.LoadUint32(&s.started) > 0
-}
+func (s *Server) initDB() error {
+	const op errors.Op = "api/Server.initDB"
 
-// Shutdown performs a graceful shutdown of the Server. Once the server has
-// been shut down, it cannot be started again.
-func (s *Server) Shutdown(ctx context.Context) error {
-	const op errors.Op = "server/server.Shutdown"
-
-	if !s.isStarted() {
-		return errors.New(op, "not started")
-	}
-
-	var errcol errors.Collection
-	errcol = errors.Append(errcol, s.httpAPIServer.Shutdown(ctx), op)
-	errcol = errors.Append(errcol, s.boltDB.Close(), op)
-	return errcol.ErrorOrNil()
-}
-
-// initialize initializes the un-exported fields of Server. It must not
-// be called more than once.
-func (s *Server) initialize() {
 	s.boltDB = &db.Bolt{
 		FilePath: filepath.Join(s.DataDir, "acmeproxy.db"),
 		FileMode: 0600,
 	}
-	acmeclient.InitializeLego(s.Logger)
-	acmeClient := &acmeclient.Client{
-		DirectoryURL: s.ACMEDirectoryURL,
-	}
-	s.acmeAgent = &acme.Agent{
-		Domains:      s.boltDB.DomainRepository(),
-		Users:        s.boltDB.UserRepository(),
-		Certificates: acmeClient,
-		ACMEAccounts: acmeClient,
-	}
-	s.httpAPIServer = &httpapi.Server{
-		Solver: &acmeClient.HTTP01Solver,
+	err := s.boltDB.Open()
+	return errors.Wrap(err, op)
+}
+
+func (s *Server) startHTTPAPI() error {
+	const op errors.Op = "api/server.startHTTPAPI"
+
+	addrC := make(chan string)
+	go errors.LogFunc(s.Logger, func() error {
+		err := netutil.ListenAndServe(s.httpAPIServer, netutil.WithAddr(s.HTTPAPIAddr), netutil.NotifyAddr(addrC))
+		return errors.Wrap(err, op)
+	})
+	select {
+	case <-addrC:
+		return nil
+	case <-time.After(100 * time.Millisecond):
+		return errors.New(op, "http API startup timed out")
 	}
 }
+
 func (s *Server) registerAcmeproxyDomain() error {
 	const op errors.Op = "server/server.registerAcmeproxyDomain"
 
@@ -119,4 +131,19 @@ func (s *Server) registerAcmeproxyDomain() error {
 		return errors.New(op, fmt.Sprintf("register acmeproxy domain: %s", "www.example.com"), err)
 	}
 	return nil
+}
+
+// Shutdown performs a graceful shutdown of the Server. Once the server has
+// been shut down, it cannot be started again.
+func (s *Server) Shutdown(ctx context.Context) error {
+	const op errors.Op = "server/server.Shutdown"
+
+	if atomic.LoadUint32(&s.started) == 0 {
+		return errors.New(op, "not started")
+	}
+
+	var errcol errors.Collection
+	errcol = errors.Append(errcol, s.httpAPIServer.Shutdown(ctx), op)
+	errcol = errors.Append(errcol, s.boltDB.Close(), op)
+	return errcol.ErrorOrNil()
 }
